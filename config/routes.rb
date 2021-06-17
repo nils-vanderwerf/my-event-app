81be2e4e84cce67dc40178563af1a3e845ee8389
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-17 10:06:03 +1000

    User successfully registered

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :views
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
