5dd82be2ae6fabdd06c4bb6cae50e79bafa69ae2
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-18 19:31:44 +1000

    Can RSVP

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :events do
+    member do
+      get 'rsvp'
+      get 'cancel_rsvp'
+    end
+  end
+
   root to: "events#index"
 end
