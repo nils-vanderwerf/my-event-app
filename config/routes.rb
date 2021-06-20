12b301ecb200b1458b692dbc2b955489fa62c87f
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-20 19:41:11 +1000

    Cancel RSVP is working again

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  
-  devise_for :views
-  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events do
     member do
       get 'rsvp'
-      get 'cancel_rsvp'
+      delete 'cancel_rsvp'
     end
   end
 
